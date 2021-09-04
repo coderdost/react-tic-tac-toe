d3c635dafe334149fc9fc01ec210d931d43b9215
Abhishek Rathore <rathore.abhishek87@gmail.com>
2021-09-04 15:03:40 +0530

    Redux added

--- src/App.js
+++ src/App.js
@@ -1,57 +1,104 @@
-import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from 'react';
-import {connect, Provider} from 'react-redux';
-import {createStore} from 'redux';
+import { useEffect } from 'react';
+import { connect, Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+// REDUX: Initial State
+const initial_state = {
+  marks: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  player: 1,
+  gameOver: false
+};
+// REDUX: Reducer
+
+const reducer = (state = initial_state, action) => {
+  switch (action.type) {
+    case 'SET_PLAYER':
+      return {...state, player: action.payload}
+    case 'SET_MARKS':
+      return {...state, marks: action.payload}
+    case 'SET_GAMEOVER':
+        return {...state, gameOver: action.payload}  
+    default:
+      return state;
+  }
+};
+// REDUX: Store
+
+const store = createStore(reducer);
+
+
+// App Component with Provider on BoardContainer
 function App() {
   return (
     <div className="App">
-      <Board></Board>
+      <Provider store={store}>
+        <BoardContainer></BoardContainer>
+      </Provider>
     </div>
   );
 }
 
-function Board() {
-const [marks,setMarks] = useState([0,0,0,0,0,0,0,0,0]);
-const [player, setPlayer] = useState(1);
-
-useEffect(()=>{
-  const combinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ]
-
-  for (let c of combinations){
-    if(marks[c[0]] ===1  && marks[c[1]] ===1 && marks[c[2]] ===1){
-      console.log('player 1 wins')
-    }
-    if(marks[c[0]] ===2  && marks[c[1]] ===2 && marks[c[2]] ===2){
-      console.log('player 2 wins')
+// BoardContainer Component using Connect and Map functions
+const mapStateToProps = (state)=>{
+  return {
+    marks : state.marks,
+    player: state.player,
+    gameOver: state.gameOver
+  }
+}
+const mapDispatchToProps = (dispatch)=>{
+  return {
+    setMarks : (marks)=>{
+      dispatch({type:'SET_MARKS',payload:marks})
+    },
+    setPlayer : (player)=>{
+      dispatch({type:'SET_PLAYER',payload:player})
+    },
+    setGameOver : (status)=>{
+      dispatch({type:'SET_GAMEOVER',payload:status})
     }
   }
+}
+const BoardContainer = connect(mapStateToProps,mapDispatchToProps)(Board);
 
-}, [marks])
+// Board Component
+function Board({marks, player,gameOver,setGameOver,setMarks,setPlayer}) {
+  
+  useEffect(() => {
+    const combinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
+    for (let c of combinations) {
+      if (marks[c[0]] === 1 && marks[c[1]] === 1 && marks[c[2]] === 1) {
+        console.log('player 1 wins');
+        setGameOver(true)
+      }
+      if (marks[c[0]] === 2 && marks[c[1]] === 2 && marks[c[2]] === 2) {
+        console.log('player 2 wins');
+        setGameOver(true)
+      }
+    }
+  }, [marks]);
 
-
-const changeMark = (i)=>{
+  const changeMark = (i) => {
     const m = [...marks];
-    if(m[i]===0){
+    if (m[i] === 0 && !gameOver) {
       m[i] = player;
       setMarks(m);
-      setPlayer(player===1 ? 2:1);
-    }else{
-      alert('please click on empty blocks')
+      setPlayer(player === 1 ? 2 : 1);
+    } else {
+      alert('please click on empty blocks');
     }
-   
-}
+  };
 
   return (
     <div className="Board">
@@ -70,17 +117,19 @@ const changeMark = (i)=>{
         <Block mark={marks[7]} position={7} changeMark={changeMark}></Block>
         <Block mark={marks[8]} position={8} changeMark={changeMark}></Block>
       </div>
-     
-     
-     
-    
     </div>
   );
 }
 
-function Block({mark, changeMark, position}) {
-  return <div className={`Block mark${mark}`}
-          onClick={e=>changeMark(position)}></div>;
+
+// Block Component
+function Block({ mark, changeMark, position }) {
+  return (
+    <div
+      className={`Block mark${mark}`}
+      onClick={(e) => changeMark(position)}
+    ></div>
+  );
 }
 
 export default App;
