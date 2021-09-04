8ef9dd4eb77882854af17c2edfa6adbdb9420418
Abhishek Rathore <rathore.abhishek87@gmail.com>
2021-09-04 13:03:24 +0530

    redux imported

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
+import {connect, Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 function App() {
   return (
